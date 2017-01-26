219c9a4f0e355b699264d85123538261d49d3363
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2017-01-26 10:16:50 +0100

    Initial commit

---
+++ WorkingWithNavigation/Page1Xaml.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace WorkingWithNavigation
+{
+	public partial class Page1Xaml : ContentPage
+	{
+		public Page1Xaml ()
+		{
+			InitializeComponent ();
+
+			Debug.WriteLine("Page1Xaml");
+		}
+
+		~Page1Xaml()
+		{
+			Debug.WriteLine("~Page1Xaml");
+		}
+
+		async void OnNextPageButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new Page2Xaml ());
+		}
+	}
+}
